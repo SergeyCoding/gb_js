87b598b8ba50f24a6efb39b9b49b0396a8fef326
SergeyXP <s.pusthotin@gmail.com>
2023-08-18 18:37:06 +0400

    hw08

--- hw08/3.js
+++ hw08/3.js
@@ -14,4 +14,21 @@
 не окажется, значит нужно будет вывести пустой массив.
 */
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+const arr3 = [];
+
+for (let i = 0; i < 5; i++) {
+  arr3[i] = Math.floor(Math.random() * 10);
+}
+console.log("task 3");
+console.log("Random array:");
+console.log(arr3);
+console.log("Sum:");
+console.log(arr3.reduce((a, x) => a + x, 0));
+console.log("Min:");
+console.log(arr3.reduce((a, x) => Math.min(a, x), Infinity));
+
+console.log("Indexes with value=3:");
+console.log(
+  arr3.map((v, i) => (v === 3 ? i : NaN)).filter((v) => Number.isFinite(v))
+);
+
