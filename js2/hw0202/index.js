20ad45d2d3baa50fdf077f9ac2db88c2d19f7273
SergeyXP <s.pusthotin@gmail.com>
2023-08-30 20:05:48 +0400

    hw0202

--- js2/hw0202/index.js
+++ js2/hw0202/index.js
@@ -9,6 +9,23 @@
 // Свойство pages (количество страниц) - число, количество страниц в книге.
 // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+
+  displayInfo() {
+    console.log(
+      `название: ${this.title}, автор: ${this.author}, количество страниц: ${this.pages} с.`
+    );
+  }
+}
+
+const b = new Book('Война и мир', 'Толстой Л.Н.', '400');
+b.displayInfo();
+
 // Задание 2: ""Управление списком студентов""
 // Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
 
@@ -32,3 +49,84 @@
 // // Name: Jane Doe
 // // Age: 17
 // // Grade: 11th grade"
+const a = {
+  n: 'nnn',
+  ff: function (params) {
+    () => this;
+  },
+};
+
+const f = () => this;
+console.log('1:', f());
+console.log('2: ', a.ff());
+console.log(a.ff.bind(a)());
+
+// Задание 1: ""Управление библиотекой книг""
+
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+// Задание 2: ""Управление списком студентов""
+// Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+// Свойство name (имя) - строка, имя студента.
+// Свойство age (возраст) - число, возраст студента.
+// Свойство grade (класс) - строка, класс, в котором учится студент.
+// Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
+// javascript
+
+// // Пример использования класса
+// const student1 = new Student(""John Smith"", 16, ""10th grade"");
+// student1.displayInfo();
+// // Вывод:
+// // Name: John Smith
+// // Age: 16
+// // Grade: 10th grade
+
+// const student2 = new Student(""Jane Doe"", 17, ""11th grade"");
+// student2.displayInfo();
+// // Вывод:
+// // Name: Jane Doe
+// // Age: 17
+// // Grade: 11th grade"
+
+// Необязательные задачи
+
+// 1
+// Создать класс "Телефонная книга" с методами для добавления, удаления и поиска контактов по имени или номеру телефона.
+
+// // Пример использования
+// let phonebook = new Phonebook();
+// phonebook.addContact("Иванов", "123-45-67");
+// phonebook.addContact("Петров", "987-65-43");
+// console.log(phonebook.findContactByName("Иванов")); // { name: "Иванов", phone: "123-45-67" }
+// console.log(phonebook.findContactByPhone("987-65-43")); // { name: "Петров", phone: "987-65-43" }
+// phonebook.deleteContact("Иванов");
+// console.log(phonebook.contacts); // [{ name: "Петров", phone: "987-65-43" }]
+
+// 2
+// Это расширенная версия задачи с банком, которую мы решлали на семинаре:
+
+// Создайте класс "Банк", который будет иметь следующие свойства: название банка, список клиентов и список счетов. Класс должен иметь методы для добавления нового клиента, открытия нового счета для клиента, пополнения счета, снятия денег со счета и проверки баланса.
+
+// Пример работы:
+
+// const bank = new Bank("Мой Банк");
+
+// const client1 = new Client("Иван", 25);
+// const client2 = new Client("Мария", 30);
+
+// bank.addClient(client1);
+// bank.addClient(client2);
+
+// bank.openAccount(client1, 1000);
+// bank.openAccount(client2, 500);
+
+// bank.deposit(123456789, 200);
+// bank.withdraw(987654321, 100);
+// bank.checkBalance(123456789);
+// bank.checkBalance(987654321);
