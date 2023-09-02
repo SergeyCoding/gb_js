cac673631a68294f975f121ebd3c765313d5bafc
SergeyXP <s.pusthotin@gmail.com>
2023-09-02 08:52:20 +0400

    hw0203

--- js2/hw0203/task01.js
+++ js2/hw0203/task01.js
@@ -19,3 +19,30 @@
 // // Вывод:
 // // Name: Jane Doe
 // // Department: Sales
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  displayInfo() {
+    console.log('Name: ', this.name);
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department) {
+    super(name);
+    this.department = department;
+  }
+
+  displayInfo() {
+    console.log(`Name: ${this.name}\nDepartment: ${this.department}`);
+  }
+}
+
+const employee = new Employee(`John Smith`);
+employee.displayInfo();
+console.log();
+const manager = new Manager('Jane Doe', 'Sales');
+manager.displayInfo();
--- js2/hw0203/task02.js
+++ js2/hw0203/task02.js
@@ -24,3 +24,35 @@
 // order.addProduct(product2);
 
 // console.log(order.getTotalPrice()); // Вывод: 600
+
+class Order {
+  products = [];
+  constructor(orderNumber) {
+    this.orderNumber = orderNumber;
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+  }
+
+  getTotalPrice() {
+    return this.products.reduce((s, x) => s + x.price, 0);
+  }
+}
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product('Phone', 500);
+order.addProduct(product1);
+
+const product2 = new Product('Headphones', 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
