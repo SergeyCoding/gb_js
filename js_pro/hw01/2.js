2ae7ab77f27a8ef60859f06c9063134c5a8c7335
SergeyXP <SergeyXP@gmail.com>
2023-10-10 08:34:22 +0400

    js_pro_hw01

--- js_pro/hw01/2.js
+++ js_pro/hw01/2.js
@@ -31,3 +31,26 @@
 Клиент Мария заказала: Суши "Калифорния" и Пиццу "Маргарита".
 Клиент Ирина заказала: Чизкейк.
 */
+
+const cooks = new Map();
+cooks.set("Виктор", "Пицца");
+cooks.set("Ольга", "Суши");
+cooks.set("Дмитрий", "Десерты");
+
+console.log(cooks);
+
+const food = new Map();
+food.set("Пицца", new Set(['Пицца "Маргарита"', 'Пицца "Пепперони"']));
+food.set("Суши", new Set(['Суши "Филадельфия"', 'Суши "Калифорния"']));
+food.set("Десерты", new Set(["Тирамису", "Чизкейк"]));
+console.log(food);
+
+const orders = [];
+orders.push({ orderId: 1, client: "Алексей", order: new Set(['Пицца "Пепперони"', "Тирамису"]) });
+orders.push({ orderId: 2, client: "Мария", order: new Set(['Суши "Калифорния"', 'Пиццу "Маргарита"']) });
+orders.push({ orderId: 3, client: "Ирина", order: new Set(["Чизкейк"]) });
+console.log(orders);
+
+function makeOrder(client, food) {}
+
+function tasksForCooks() {}
