7da5ee7c7ddaa83038919f1567091399c390ebe9
SergeyXP <s.pusthotin@gmail.com>
2023-10-17 11:28:48 +0400

    js_pro_hw03

--- js_pro/hw03/js/goods.js
+++ js_pro/hw03/js/goods.js
@@ -1,97 +1,134 @@
 'use strict';
 
-const GOODS = 'goods';
+class GoodsManager {
+  constructor() {}
+
+  getGoods() {
+    try {
+      return JSON.parse(localStorage.getItem('goods')).data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  setGoods(goods) {
+    return localStorage.setItem('goods', JSON.stringify({ data: goods }));
+  }
+}
+
+class ReviewManager {
+  constructor(goods) {
+    this.goods = goods;
+  }
+
+  getReviews() {
+    try {
+      return JSON.parse(localStorage.getItem(`goods{${this.goods}}`)).data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  setReviews(reviews) {
+    return localStorage.setItem(`goods{${this.goods}}`, JSON.stringify({ data: reviews }));
+  }
+}
 
 const selectGoods = document.querySelector('#selectGoodsId');
 const currentProduct = document.querySelector('#currentProductId');
+const currentReview = document.querySelector('#currentReviewId');
 const addButton = document.querySelector('#addButtonId');
+const showReview = document.querySelector('#showReviewId');
 
-const initialData = [
-  {
-    product: 'Apple iPhone 13',
-    reviews: [
-      {
-        id: 1,
-        text: 'Отличный телефон! Батарея держится долго.',
-      },
-      {
-        id: 2,
-        text: 'Камера супер, фото выглядят просто потрясающе.',
-      },
-    ],
-  },
-  {
-    product: 'Samsung Galaxy Z Fold 3',
-    reviews: [
-      {
-        id: 3,
-        text: 'Интересный дизайн, но дорогой.',
-      },
-    ],
-  },
-  {
-    product: 'Sony PlayStation 5',
-    reviews: [
-      {
-        id: 4,
-        text: 'Люблю играть на PS5, графика на высоте.',
-      },
-    ],
-  },
-];
+const gs = new GoodsManager();
+console.log(gs.getGoods());
 
-localStorage.clear();
-
-if (localStorage.getItem(GOODS) === null) {
-  localStorage.setItem(GOODS, JSON.stringify({ data: initialData }));
+if (gs.getGoods().length === 0) {
+  gs.setGoods(initialData().map((x) => x.product));
+  console.log('init', gs.getGoods());
 }
 
-const goodsData = JSON.parse(localStorage.getItem(GOODS)).data;
-
-goodsData.forEach((x) => {
-  const opt = document.createElement('option');
-  opt.innerText = x.product;
-  selectGoods.appendChild(opt);
+gs.getGoods().forEach((x) => {
+  const option = document.createElement('option');
+  option.innerText = x;
+  selectGoods.appendChild(option);
 });
 
 selectGoods.addEventListener('change', (e) => {
-  currentProduct.value = e.target.value;
+  if (currentProduct) {
+    currentProduct.value = e.target.value;
+  }
 });
 
-// for (const item of initialData) {
-//   item.reviews.forEach((x) => addReview({ product: item.product, review: x.text }, false));
-// }
+addButton?.addEventListener('click', () => {
+  const set = new Set(gs.getGoods());
 
-// document.querySelector('.send-button').addEventListener('click', sendMessage);
-
-function sendMessage() {
-  const product = document.querySelector('.send-product').value;
-  const review = document.querySelector('.send-message').value;
-  addReview({ product, review });
-}
-
-/**
- *
- * @param {{product:string, review:string}} message
- * @param {boolean} isValidate
- */
-function addReview(message, isValidate = true) {
-  if (isValidate && message.product.length < 1) {
-    throw new Error('Не указан товар');
+  if (!set.has(currentProduct.value)) {
+    set.add(currentProduct.value);
+    gs.setGoods(Array.from(set));
+    console.log(gs.getGoods());
   }
 
-  if (isValidate && message.review.length < 50) {
-    throw new Error('Длина введенного отзыва менее 50 символов');
+  console.log('currentReview', currentReview.value);
+  const rs = new ReviewManager(currentProduct.value);
+  const setReviews = new Set(rs.getReviews());
+  setReviews.add(currentReview.value);
+  rs.setReviews(Array.from(setReviews));
+  console.log(rs.getReviews());
+});
+
+showReview?.addEventListener('click', () => {
+  document.querySelector('.review-container').innerText = '';
+
+  const rs = new ReviewManager(selectGoods.value);
+
+  if (rs.getReviews().length === 0) {
+    document.querySelector('.review-container').innerText = 'Отзывов нет';
+    return;
   }
 
-  if (isValidate && message.review.length > 500) {
-    throw new Error('Длина введенного отзыва более 500 символов');
+  for (const item of rs.getReviews()) {
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `<p>${item}</p>`;
+
+    document.querySelector('.review-container').append(newDiv);
   }
+});
 
-  const sc = document.querySelector('.send-container');
-
-  const newDiv = document.createElement('div');
-  newDiv.innerHTML = `<h1>${message.product}</h1><p>${message.review}</p>`;
-
-  sc.append(newDiv);
+function initialData() {
+  return [
+    {
+      product: 'Apple iPhone 13',
+      reviews: [
+        {
+          id: 1,
+          text: 'Отличный телефон! Батарея держится долго.',
+        },
+        {
+          id: 2,
+          text: 'Камера супер, фото выглядят просто потрясающе.',
+        },
+      ],
+    },
+    {
+      product: 'Samsung Galaxy Z Fold 3',
+      reviews: [
+        {
+          id: 3,
+          text: 'Интересный дизайн, но дорогой.',
+        },
+      ],
+    },
+    {
+      product: 'Sony PlayStation 5',
+      reviews: [
+        {
+          id: 4,
+          text: 'Люблю играть на PS5, графика на высоте.',
+        },
+      ],
+    },
+  ];
 }
