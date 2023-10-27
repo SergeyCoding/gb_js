85a27345f04afb9500b7453d3f417de4af5f4929
SergeyXP <s.pusthotin@gmail.com>
2023-10-27 19:24:41 +0400

    js_dom_hw02

--- js_dom/hw02/js/main.js
+++ js_dom/hw02/js/main.js
@@ -1,36 +1,28 @@
-import { Slider } from './Slider.js';
-import { SpinnerIndicator } from './SpinnerIndicator.js';
+import { SliderIndicator } from './SliderIndicator.js';
 
 const pictures = ['pic_01.avif', 'pic_02.avif', 'pic_03.avif'];
-
-const slider = new Slider(pictures.length);
 
 const img = document.querySelector('.container img');
 
 const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
-const spinnerBlock = document.querySelector('.spinner-block');
+const sliderBlock = document.querySelector('.slider-block');
 
-const indicator = new SpinnerIndicator(spinnerBlock, pictures.length);
+const indicator = new SliderIndicator(sliderBlock, pictures.length);
 indicator.drawInit();
 
 prevBtn.onclick = () => {
-  slider.prev();
-  changePicture(slider.current);
-  indicator.setSelected(slider.current);
+  indicator.prev();
 };
 
 nextBtn.onclick = () => {
-  slider.next();
-  changePicture(slider.current);
-  indicator.setSelected(slider.current);
+  indicator.next();
 };
 
-document.addEventListener('spinner-click', (e) => {
-  slider.current = e.spinnerPoint;
-  changePicture(slider.current);
+document.addEventListener('slider-click', (e) => {
+  img.src = './img/' + pictures[e.sliderPoint];
 });
 
-function changePicture(n) {
-  img.src = './img/' + pictures[n];
-}
+setInterval(() => {
+  indicator.next();
+}, 3000);
